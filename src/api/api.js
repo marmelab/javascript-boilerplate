e09f5baa61e0f7d3e06924360dc8b42a51b0c45c
alexisjanvier <contact@alexisjanvier.net>
2016-02-12 18:02:47 +0100

    Remove auth from public api
    it is temporary

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,14 @@
 import koa from 'koa';
 import koaMount from 'koa-mount';
 
-import tokenCheckerMiddleware from './middlewares/tokenChecker';
-
 import authenticateRoutes from './users/authenticateRoutes';
 import orderApiRoutes from './orders/orderApiRoutes';
 import productApiRoutes from './products/productApiRoutes';
-import userApiRoutes from './users/userApiRoutes';
 
 const app = koa();
 
 app.use(koaMount('/authenticate', authenticateRoutes));
-app.use(tokenCheckerMiddleware);
 app.use(koaMount('/orders', orderApiRoutes));
 app.use(koaMount('/products', productApiRoutes));
-app.use(koaMount('/users', userApiRoutes));
 
 export default app;
