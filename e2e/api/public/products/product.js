65a2fc4a5adbaf0e5422482d39dfeea18076b8eb
alexisjanvier <contact@alexisjanvier.net>
2016-02-13 18:32:35 +0100

    specific product entry point with tests

---
+++ e2e/api/public/products/product.js
@@ -0,0 +1,54 @@
+/* eslint func-names:0 no-undef:0*/
+
+describe('/api/products/{id}', () => {
+    let product;
+    before(function* addFixtures() {
+        product = yield fixtureLoader.addProduct();
+    });
+    describe('GET', () => {
+        it('should not require authentification', function* shouldNotRequireAuthentification() {
+            const { statusCode } = yield request({
+                method: 'GET',
+                url: `/api/products/${product.id}`,
+            });
+            assert.equal(statusCode, 200);
+        });
+        it('should return information about a specific products', function* shouldReturnAllProductsList() {
+            const { body } = yield request({
+                method: 'GET',
+                url: `/api/products/${product.id}`,
+            });
+            assert.deepEqual(body, product);
+        });
+    });
+    describe('POST', () => {
+        it('should not allow POST request', function* () {
+            const { statusCode } = yield request({
+                method: 'POST',
+                url: `/api/products/${product.id}`,
+            });
+            assert.equal(statusCode, 405);
+        });
+    });
+    describe('PUT', () => {
+        it('should not allow PUT request', function* () {
+            const { statusCode } = yield request({
+                method: 'PUT',
+                url: `/api/products/${product.id}`,
+            });
+            assert.equal(statusCode, 405);
+        });
+    });
+    describe('DELETE', () => {
+        it('should not allow DELETE request', function* () {
+            const { statusCode } = yield request({
+                method: 'DELETE',
+                url: `/api/products/${product.id}`,
+            });
+            assert.equal(statusCode, 405);
+        });
+    });
+    after(function* removeFixtures() {
+        yield fixtureLoader.removeAllFixtures();
+    });
+});
--- e2e/lib/fixturesLoader.js
+++ e2e/lib/fixturesLoader.js
@@ -1,17 +1,40 @@
+import faker from 'faker';
+import uuid from 'uuid';
+
 import data from '../fixtures/demo_fixtures.json';
 import productFactory from '../../src/api/products/productModel';
 
 export default function(client) {
     const productQueries = productFactory(client);
+
     function* loadDefaultFixtures() {
         yield productQueries.batchInsert(data.products);
     }
+
     function* removeAllFixtures() {
         yield client.query_('TRUNCATE product CASCADE');
+    }
+
+    function* addProduct(productData) {
+        // const causes an error! don't know why
+        let defaultProductData = {
+            reference: uuid.v1(),
+            width: 60,
+            height: 40,
+            price: faker.random.number(),
+            thumbnail: faker.image.imageUrl(60, 60),
+            image: faker.image.imageUrl(400, 400),
+            description: faker.lorem.sentence(),
+            stock: faker.random.number(),
+        };
+
+        // ES7, in Babel it is experimental stage 2 and enabled by default
+        return yield productQueries.insertOne({ ...defaultProductData, ...productData });
     }
 
     return {
         loadDefaultFixtures,
         removeAllFixtures,
+        addProduct,
     };
 }
