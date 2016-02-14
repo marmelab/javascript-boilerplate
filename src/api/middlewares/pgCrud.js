5373c06f0a283d3ca5c4e688de684dafa8b80377
alexisjanvier <contact@alexisjanvier.net>
2016-02-14 14:12:02 +0100

    Refactoring CRUD lib as moddleware

---
+++ src/api/middlewares/pgCrud.js
@@ -0,0 +1,129 @@
+/* eslint func-names:0*/
+
+import koa from 'koa';
+import coBody from 'co-body';
+import koaRoute from 'koa-route';
+
+export default (queriesFactory, availableMethods = ['GET', 'POST', 'PUT', 'DELETE']) => {
+    const app = koa();
+    let queries;
+
+    app.use(function* (next) {
+        const clean = method => method.toLowerCase().trim();
+        const method = clean(this.method);
+        this.isAllowed = availableMethods.map(clean).indexOf(method) !== -1;
+
+        yield next;
+    });
+
+    app.use(function* (next) {
+        queries = queriesFactory(this.client);
+        yield next;
+    });
+
+    // GET /
+    app.use(koaRoute.get('/', function* (next) {
+        if (this.isAllowed) {
+            const query = this.request.query;
+            const excludedQueryParams = ['limit', 'offset', 'filter', '_sort', '_sortDir'];
+            const other = {};
+            Object.keys(query).forEach(key => {
+                if (excludedQueryParams.indexOf(key) !== -1) return;
+                other[key] = query[key];
+            });
+
+            this.body = yield queries.selectPage(query.limit, query.offset, query.filter, query._sort, query._sortDir, other);
+            const totalCount = (this.body[0]) ? this.body[0].totalcount : (yield queries.countAll());
+            this.set('X-Total-Count', totalCount);
+            this.set('Access-Control-Expose-Headers', 'X-Total-Count');
+        }
+
+        yield next;
+    }));
+
+    // GET /:id
+    app.use(koaRoute.get('/:id', function* (id, next) {
+        if (this.isAllowed) {
+            try {
+                this.body = yield queries.selectOneById(id);
+            } catch (e) {
+                if (e.message === 'not found') {
+                    this.status = 404;
+                } else {
+                    throw e;
+                }
+            }
+        }
+        yield next;
+    }));
+
+    // POST /
+    app.use(koaRoute.post('/', function* (next) {
+        if (this.isAllowed) {
+            const data = this.data || (yield coBody(this));
+            this.body = yield queries.insertOne(data);
+        }
+
+        yield next;
+    }));
+
+    // POST /multi
+    app.use(koaRoute.post('/multi', function* (next) {
+        if (this.isAllowed) {
+            const data = this.data || (yield coBody(this));
+            this.body = yield queries.batchInsert(data);
+        }
+
+        yield next;
+    }));
+
+    // DELETE /
+    app.use(koaRoute.delete('/:id', function* (id, next) {
+        if (this.isAllowed) {
+            try {
+                this.body = yield queries.removeOne(id);
+            } catch (e) {
+                if (e.message === 'not found') {
+                    this.status = 404;
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        yield next;
+    }));
+
+    // DELETE /multi
+    app.use(koaRoute.delete('/multi', function* (next) {
+        if (this.isAllowed) {
+            const ids = this.query.id;
+            this.body = yield queries.batchDelete(ids);
+        }
+
+        yield next;
+    }));
+
+    // PUT /:id
+    app.use(koaRoute.put('/:id', function* (id, next) {
+        if (this.isAllowed) {
+            const data = this.data || (yield coBody(this));
+            let modifiedEntity;
+            try {
+                modifiedEntity = yield queries.updateOne(id, data);
+            } catch (e) {
+                if (e.message === 'not found') {
+                    this.status = 404;
+                    return;
+                }
+
+                throw e;
+            }
+            this.body = modifiedEntity;
+        }
+
+        yield next;
+    }));
+
+    return app;
+};
