0735c6ae8a29de36e6b1344ea9c8943cc2c4ff3b
alexisjanvier <contact@alexisjanvier.net>
2016-02-14 14:16:07 +0100

    Using requestFilter Middleware on api

--- src/api/api.js
+++ src/api/api.js
@@ -3,28 +3,18 @@ import koaMount from 'koa-mount';
 import koaRoute from 'koa-route';
 
 import authenticateRoutes from './users/authenticateRoutes';
+import methodFilter from './middlewares/methodFilter';
 import orderApiRoutes from './orders/orderApiRoutes';
 import productApiRoutes from './products/productApiRoutes';
 
 const app = koa();
 
 app.use(koaMount('/authenticate', authenticateRoutes));
-app.use(koaMount('/orders', orderApiRoutes));
 app.use(koaMount('/products', productApiRoutes));
+app.use(koaMount('/orders', orderApiRoutes));
 
-app.use(function* filterRequestMethod(next) {
-    const clean = method => method.toLowerCase().trim();
-    const method = clean(this.method);
 
-    if (method !== 'get') {
-        this.status = 405;
-        this.body = {};
-        return;
-    }
-
-    yield next;
-});
-
+app.use(methodFilter(['GET']));
 app.use(koaRoute.get('/', function* primaryEntryPoint() {
     this.status = 200;
     this.body = {status: 'ok'};
