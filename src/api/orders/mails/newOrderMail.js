43c2d18d9a2b313f32d6c786a64958e6433674b9
Kmaschta <kevin@marmelab.com>
2016-03-03 16:05:43 +0100

    Remove ejs as dependency

--- src/api/orders/mails/newOrderMail.js
+++ src/api/orders/mails/newOrderMail.js
@@ -1,9 +1,22 @@
-import 'ejs';
+export const template = (subject, order) => `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>${subject}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <p>Dear Customer</p>
+        <p>Your order "${order.reference}" has successfuly be send.</p>
+    </body>
+</html>
+`;
+
 
 export default function prepareNewOrderMail(user, order) {
     const subject = 'Your order is send';
-    const compileTemplate = require('./newOrderMail.ejs');
-    const body = compileTemplate({ subject, user, order });
+    const body = template(subject, order);
 
     return {
         to: user.email,
