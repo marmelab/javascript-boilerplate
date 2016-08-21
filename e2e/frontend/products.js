c28b609debc7335c109fab0d3e08a90ad2dea81f
djhi <gildas.garcia@gmail.com>
2016-08-21 16:20:34 +0200

    Fix nodium usage to prevent random e2e frontend tests failures

--- e2e/frontend/products.js
+++ e2e/frontend/products.js
@@ -1,19 +1,24 @@
+import { By } from 'selenium-webdriver';
 import { after, describe, it } from 'selenium-webdriver/testing';
 import driver from 'nodium/lib/driver';
+import utils from 'nodium/lib/driver/utils';
 import chai, { expect } from 'chai';
+
 chai.use(require('chai-as-promised'));
 chai.use(require('chai-webdriver')(driver));
 
 describe('Products', function () { // eslint-disable-line func-names
-    this.timeout(10000);
+    this.timeout(15000);
 
     it('user should see the product list', () => {
         driver.get('http://localhost:8081/#/products');
+        utils(driver).waitForElementVisible(By.css('.product-item'));
         expect('.product-item').dom.to.have.count(3);
     });
 
     it('user should see the product details', () => {
         driver.get('http://localhost:8081/#/products/1');
+        utils(driver).waitForElementVisible(By.css('.product-details'));
         expect('.product-details').dom.to.be.visible();
         expect('.img-thumbnail').dom.to.be.visible();
         expect('.img-thumbnail').dom.to.have.attribute('src', 'http://lorempixel.com/400/400/');
