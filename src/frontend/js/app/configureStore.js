af161c64152afc62691dc55ffc9d3cbd27eb5bd3
djhi <gildas.garcia@gmail.com>
2016-09-04 15:46:51 +0200

    [RFR] Fix frontend by completely removing old redux devtools

--- src/frontend/js/app/configureStore.js
+++ src/frontend/js/app/configureStore.js
@@ -3,29 +3,20 @@ import { routerMiddleware } from 'react-router-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import thunkMiddleware from 'redux-thunk';
-
 import sagas from './sagas';
+
 const sagaMiddleware = createSagaMiddleware();
 
 export default function configureStore(rootReducer, initialState) {
+    const middlewares = applyMiddleware(
+        sagaMiddleware,
+        routerMiddleware(hashHistory),
+        thunkMiddleware
+    );
 
-    let enhancers = [
-        applyMiddleware(
-            sagaMiddleware,
-            routerMiddleware(hashHistory),
-            thunkMiddleware
-        ),
-    ];
+    const devtools = window.devToolsExtension ? window.devToolsExtension() : f => f;
 
-    if (FRONTEND__APP__ENABLE_DEV_TOOLS) { // eslint-disable-line no-undef
-        const DevTools = require('./DevTools');
-        enhancers = [
-            ...enhancers,
-            DevTools.instrument(),
-        ];
-    }
-
-    const store = createStore(rootReducer, initialState, compose(...enhancers));
+    const store = createStore(rootReducer, initialState, compose(middlewares, devtools));
     sagaMiddleware.run(sagas);
     return store;
 }
