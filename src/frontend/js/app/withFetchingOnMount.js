61e4905c13a7c850fdd4c5fc1842eb18fec41553
djhi <gildas.garcia@gmail.com>
2016-09-08 09:07:42 +0200

    review

--- src/frontend/js/app/withFetchingOnMount.js
+++ src/frontend/js/app/withFetchingOnMount.js
@@ -10,44 +10,43 @@ export const DefaultLoading = () => (
     </div>
 );
 
-export default (action, dataStateSelector, paramsStateSelector, loadingStateSelector, LoadingComponent = DefaultLoading) => BaseComponent => {
-    // This will return the component correctly initialized
-    const factory = createEagerFactory(BaseComponent);
+export default (action, dataStateSelector, paramsStateSelector, loadingStateSelector, LoadingComponent = DefaultLoading) => // eslint-disable-line max-len
+    BaseComponent => {
+        // This will return the component correctly initialized
+        const factory = createEagerFactory(BaseComponent);
 
-    const mapStateToProps = (state, ownProps) => ({
-        data: dataStateSelector(state, ownProps),
-        loading: loadingStateSelector(state, ownProps),
-        params: paramsStateSelector && paramsStateSelector(state, ownProps),
-    });
+        const mapStateToProps = (state, ownProps) => ({
+            data: dataStateSelector(state, ownProps),
+            loading: loadingStateSelector(state, ownProps),
+            params: paramsStateSelector && paramsStateSelector(state, ownProps),
+        });
 
-    const mapDispatchToProps = dispatch => bindActionCreators({
-        action,
-    }, dispatch);
+        const mapDispatchToProps = dispatch => bindActionCreators({
+            action,
+        }, dispatch);
 
-    class WithActionOnMount extends Component {
-        componentDidMount() {
-            this.props.action(this.props.params);
-        }
-
-        render() {
-            const { loading, data, ...props } = this.props;
-
-            if (loading || !data) {
-                return <LoadingComponent />;
+        class WithActionOnMount extends Component {
+            componentDidMount() {
+                this.props.action(this.props.params);
             }
 
-            return factory({
-                ...props,
-            });
+            render() {
+                const { loading, data, ...props } = this.props;
+
+                if (loading || !data) {
+                    return <LoadingComponent />;
+                }
+
+                return factory(props);
+            }
         }
-    }
 
-    WithActionOnMount.propTypes = {
-        action: PropTypes.func.isRequired,
-        data: PropTypes.any,
-        params: PropTypes.any,
-        loading: PropTypes.bool.isRequired,
+        WithActionOnMount.propTypes = {
+            action: PropTypes.func.isRequired,
+            data: PropTypes.any,
+            params: PropTypes.any,
+            loading: PropTypes.bool.isRequired,
+        };
+
+        return connect(mapStateToProps, mapDispatchToProps)(WithActionOnMount);
     };
-
-    return connect(mapStateToProps, mapDispatchToProps)(WithActionOnMount);
-};
--- src/frontend/js/order/selectors.js
+++
@@ -1,2 +0,0 @@
-/* eslint import/prefer-default-export: off */
-export const getOrderById = (state, orderId) => (state.order.list.length ? undefined : state.order.list.find(p => p.id === orderId));
