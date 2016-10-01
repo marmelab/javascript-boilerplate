85c190462c425775410c5f58dca29a183644d9b4
djhi <gildas.garcia@gmail.com>
2016-10-01 10:50:14 +0200

    [RFR] Code reorganization and production optimizations - [x] Removed `common-client` in favor of `isomorphic` directory - [x] Each project now have its own `.eslintrc` - [x] Added default `staging` configuration - [x] Optimized webpack build for production (Made sure NODE_ENV was set to get react and redux in production mode)

--- src/isomorphic/fetch/withFetchingOnMount.js
+++ src/isomorphic/fetch/withFetchingOnMount.js
@@ -32,71 +32,48 @@ export const DefaultLoading = () => (
  * This HOC can be used when a component needs to fetch data on mount.
  *
  * @param actionCreator An action creator
- * @param {Object} selectors The selectors used for data, loading and fetch parameters retrieval from state.
- * @param {dataSelector} selectors.dataSelector Mandatory selector to retrieve the fetched data from state.
- * @param {loadingSelector} selectors.loadingSelector Mandatory selector to retrieve the loading status from state.
- * @param {paramsSelector} selectors.paramsSelector Optional selector to retrieve the parameters to use for fetch, from state.
+ * @param {dataSelector} dataSelector A selector to retrieve the fetched data from state.
+ * @param {paramsSelector} paramsSelector A selector to retrieve the parameters to use for fetch, from state.
+ * @param {loadingSelector} loadingSelector A selector to retrieve the loading status from state.
  * @param {Object} LoadingComponent The component to display when loading.
  */
-export default (actionCreator, selectors: {}, LoadingComponent = DefaultLoading) => BaseComponent => {
-    if (!selectors) {
-        console.error(`withFetchingOnMount(${BaseComponent.displayName}) requires at least two selectors, one to retrieve the fetched data and the other to retrieve the loading state.`); // eslint-disable-line max-len, no-console
-        if (process.env.NODE_ENV !== 'production') {
-            console.log('Pass them withing an object as the second parameter of the withFetchingOnMount call like: `withFetchingOnMount(actionCreator, { dataSelector, loadingSelector })`'); // eslint-disable-line max-len, no-console
-        }
-    }
+export default (actionCreator, dataSelector, paramsSelector, loadingSelector, LoadingComponent = DefaultLoading) => // eslint-disable-line max-len
+    BaseComponent => {
+        // This will return the component correctly initialized
+        const factory = createEagerFactory(BaseComponent);
 
-    const { dataSelector, loadingSelector, paramsSelector } = selectors;
+        const mapStateToProps = (state, ownProps) => ({
+            data: dataSelector(state, ownProps),
+            loading: loadingSelector(state, ownProps),
+            params: paramsSelector && paramsSelector(state, ownProps),
+        });
 
-    if (!dataSelector) {
-        console.error(`withFetchingOnMount(${BaseComponent.displayName}) requires the data selector, which retrieve the fetched data from state.`); // eslint-disable-line max-len, no-console
-        if (process.env.NODE_ENV !== 'production') {
-            console.log('Pass it withing an object as the second parameter of the withFetchingOnMount call like: `withFetchingOnMount(actionCreator, { dataSelector, loadingSelector })`'); // eslint-disable-line max-len, no-console
-        }
-    }
+        const mapDispatchToProps = dispatch => bindActionCreators({
+            action: actionCreator,
+        }, dispatch);
 
-    if (!loadingSelector) {
-        console.error(`withFetchingOnMount(${BaseComponent.displayName}) requires the loading selector, which retrieve the loading status from state.`); // eslint-disable-line max-len, no-console
-        if (process.env.NODE_ENV !== 'production') {
-            console.log('Pass it withing an object as the second parameter of the withFetchingOnMount call like: `withFetchingOnMount(actionCreator, { dataSelector, loadingSelector })`'); // eslint-disable-line max-len, no-console
-        }
-    }
-
-    // This will return the component correctly initialized
-    const factory = createEagerFactory(BaseComponent);
-
-    const mapStateToProps = (state, ownProps) => ({
-        data: dataSelector && dataSelector(state, ownProps),
-        loading: dataSelector && loadingSelector(state, ownProps),
-        params: paramsSelector && paramsSelector(state, ownProps),
-    });
-
-    const mapDispatchToProps = dispatch => bindActionCreators({
-        action: actionCreator,
-    }, dispatch);
-
-    class WithActionOnMount extends Component {
-        componentDidMount() {
-            this.props.action(this.props.params);
-        }
-
-        render() {
-            const { loading, data, ...props } = this.props;
-
-            if (loading || !data) {
-                return <LoadingComponent />;
+        class WithActionOnMount extends Component {
+            componentDidMount() {
+                this.props.action(this.props.params);
             }
 
-            return factory(props);
+            render() {
+                const { loading, data, ...props } = this.props;
+
+                if (loading || !data) {
+                    return <LoadingComponent />;
+                }
+
+                return factory(props);
+            }
         }
-    }
 
-    WithActionOnMount.propTypes = {
-        action: PropTypes.func.isRequired,
-        data: PropTypes.any,
-        params: PropTypes.any,
-        loading: PropTypes.bool.isRequired,
+        WithActionOnMount.propTypes = {
+            action: PropTypes.func.isRequired,
+            data: PropTypes.any,
+            params: PropTypes.any,
+            loading: PropTypes.bool.isRequired,
+        };
+
+        return connect(mapStateToProps, mapDispatchToProps)(WithActionOnMount);
     };
-
-    return connect(mapStateToProps, mapDispatchToProps)(WithActionOnMount);
-};
