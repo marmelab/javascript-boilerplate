26647395160d6515aa9612fa6e64aa54eaad30f8
ThieryMichel <thiery@marmelab.com>
2016-10-19 13:40:47 +0200

    unskip healthcare

--- e2e/api/public/healthcare.js
+++ e2e/api/public/healthcare.js
@@ -3,7 +3,7 @@ import { assert } from 'chai';
 
 import request from '../../lib/request';
 
-describe.skip('/healthcare', () => {
+describe('/healthcare', () => {
     describe('GET', () => {
         it('should return an object describing the API health', function* () {
             const { statusCode, body } = yield request({
